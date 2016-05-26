654d732e24f0fe529a0e5c016f5ac97f71a0ed05
Diana Du <dianadujing@hotmail.com>
2016-05-26 09:55:42 +0800

    updated js files and added readme

--- target/classes/com/des/data/orders.js
+++ target/classes/com/des/data/orders.js
@@ -1,24 +1,52 @@
 exports.data = {
-	orders : [ {
-		"count" : "<count>",
-		"orderId" : "001",
-		"orderTime" : "<orderTime>",
-		"payment" : "<payment>",
-		"totalPrice" : "<totalPrice>",
-		"userId" : "<userId>"
+	orders : [{
+		"count": "5",
+		"orderId": "11776421",
+		"orderTime": "2015-11-21 16:38:11",
+		"payment": "1",
+		"totalPrice": "order_amount",
+		"userId": "4715672"
 	}, {
-		"count" : "<count>",
-		"orderId" : "002",
-		"orderTime" : "<orderTime>",
-		"payment" : "<payment>",
-		"totalPrice" : "<totalPrice>",
-		"userId" : "<userId>"
+		"count": "5",
+		"orderId": "8022589",
+		"orderTime": "2015-09-08 14:24:25",
+		"payment": "1",
+		"totalPrice": "order_amount",
+		"userId": "4715672"
 	}, {
-		"count" : "<count>",
-		"orderId" : "003",
-		"orderTime" : "<orderTime>",
-		"payment" : "<payment>",
-		"totalPrice" : "<totalPrice>",
-		"userId" : "<userId>"
-	} ]
+		"count": "5",
+		"orderId": "7645897",
+		"orderTime": "2015-09-01 10:04:29",
+		"payment": "1",
+		"totalPrice": "order_amount",
+		"userId": "4715672"
+	}, {
+		"count": "5",
+		"orderId": "7166727",
+		"orderTime": "2015-08-25 15:42:11",
+		"payment": "1",
+		"totalPrice": "order_amount",
+		"userId": "4715672"
+	}, {
+		"count": "5",
+		"orderId": "6957474",
+		"orderTime": "2015-08-22 14:04:41",
+		"payment": "1",
+		"totalPrice": "order_amount",
+		"userId": "4715672"
+	}, {
+		"count": "5",
+		"orderId": "6842991",
+		"orderTime": "2015-08-20 17:21:46",
+		"payment": "1",
+		"totalPrice": "order_amount",
+		"userId": "4715672"
+	}, {
+		"count": "5",
+		"orderId": "6473454",
+		"orderTime": "2015-08-15 16:44:00",
+		"payment": "1",
+		"totalPrice": "order_amount",
+		"userId": "4715672"
+	}]
 };
